5b357643a67e9600e293554c74d4d2ff4eb4fbd5
Alxay <alxay2137@gmail.com>
2026-07-17 09:32:42 +0200

    I've modified the script to use a virtual environment (venv) instead of the system Python installation.

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-if kill "$(pgrep python -af | grep 'mods/The-Binding-of-Isaac-Rebirth-discord-rich-presence/main.py' | awk '{print $1}')"; then
+if kill "$(pgrep python -af | grep 'mods/the-binding-of-isaac-rebirth-discord-rich-presence/main.py' | awk '{print $1}')"; then
 	echo "existing process was killed"
 fi
-cd "$STEAM_COMPAT_INSTALL_PATH"
-python mods/The-Binding-of-Isaac-Rebirth-discord-rich-presence/main.py > /testfile &
+
+cd "$(dirname "$0")" || exit 1
+./venv/bin/python main.py &
+
+
+
